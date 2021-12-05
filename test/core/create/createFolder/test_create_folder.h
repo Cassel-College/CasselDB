17b839847896a1801f5dc5cb31cfd3aad5b02620
liupeng <liupeng.0@outlook.com>
2021-12-06 00:37:56 +0800

    add: add unit test by google test

---
+++ test/core/create/createFolder/test_create_folder.h
@@ -0,0 +1,26 @@
+#ifndef TEST_CORE_CREATE_CREATE_FOLDER_H
+#define TEST_CORE_CREATE_CREATE_FOLDER_H
+
+#include "gtest/gtest.h"
+
+namespace test {
+namespace test_core {
+namespace test_create {
+
+class TestCraeteFolder : public ::testing::Test{
+
+    void SetUp() override {
+
+    }
+    
+    void TearDown() override {
+
+    }
+
+};
+
+}; // namespace create
+}; // namespace core
+}; // namespace test
+
+#endif
