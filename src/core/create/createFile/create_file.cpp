bb6118cb9644aa1316cd0d2de1a3ecd22d8bb931
liupeng <liupeng.0@outlook.com>
2021-12-12 08:46:48 +0800

    Add: add create filewq

--- src/core/create/createFile/create_file.cpp
+++ src/core/create/createFile/create_file.cpp
@@ -1,4 +1,8 @@
 #include "create_file.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
 
 namespace core {
 namespace create {
@@ -42,3 +46,75 @@ CreateFileStatus core::create::CreateFile::GetStatus() const {
 }; //namespace create 
 }; //namespace core
 
+
+void core::create::CreateFile::DoCreateFile() {
+    int key = 0;
+    std::string newFolderPath;
+    // check file name
+    std::string::size_type index = this->fileName.find("/");
+    if (index == this->fileName.npos) {
+        key = 0;
+        // check folder path right.
+    } else {
+        key = 1;
+        this->status == CreateFileStatus::FILE_NAME_ERROR;
+        // check folder name error, has error char in name.
+    }
+
+    // check file path
+    if (key == 0) {
+        bool checkFolderPathKey = CreateFile::HasFile(this->path);
+        if (!checkFolderPathKey) {
+            this->status = CreateFileStatus::FILE_PATH_NOT_EXITS;
+            key = 1;
+        }
+    }
+
+    // create file
+    if (key == 0) {
+        //create file
+        bool key = CreateFile::CreateFileCore(this->path);
+        // Setting create status
+        if (key == true) {
+            this->status = CreateFileStatus::CREATE_SUCCESS;
+        } else {
+            this->status = CreateFileStatus::CREATE_ERROR;
+        }
+    }
+}
+
+bool core::create::CreateFile::HasFile(const std::string& path) {
+    bool key = false;
+    if (path.length() > 2048) {
+        return key;
+    }
+    // Liunx create folder
+    int returnKey = access(path.c_str(), F_OK);
+
+    if (returnKey == 0) {
+        key = true;
+    } else {
+        key = false;
+    }
+    return key;
+}
+
+bool core::create::CreateFile::CreateFileCore(const std::string& path) {
+    bool key = false;
+    if ((path.length() > 2048) || (path.length() == 0)) {
+        return key;
+    }
+    // check file exist
+    bool key = CreateFile::HasFile(path);
+    if (CreateFile::HasFile(path)) {
+        key = false;
+    } else {
+        FILE *file = fopen(path.c_str(), "a+");
+        if (file == nullptr) {
+            key = false;
+        } else {
+            key = true;
+        }
+    }
+    return key;
+}
